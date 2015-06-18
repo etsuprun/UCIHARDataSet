c8c92763178af2b8d244c9e54bdea967cee0a45c
Eugene Tsuprun <etsuprun@gmail.com>
2015-06-18 13:14:15 -0400

    Codebook and script

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# Hello world
+# README
+
+## What this does
+
+This script, _run_analysis.R_, transforms the raw HAR dataet (Human Activity Recognition Using Smartphones Dataset) into a tidy set. This repo also includes the resulting tidy dataset (output.txt) and the codebook (codebook.MD).
+
+The script was written by Eugene Tsuprun for a course project (Getting and Cleaning Data, https://www.coursera.org/course/getdata, June 2015).
+
+## About the raw data set
+
+The full description of the raw data set is available here: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+The raw set contains 561 measurements of velocity and acceleration, recorded with a smartphone gyroscope and accelerometer, from 30 subjects each performing 6 different activities (ALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING).
+
+## The tidy set
+
+The tidy set contains just the means and standard deviations of each measurement, by subject and by activity. See _codebook.MD_ for details.
+
+## The script
+
+The script, _run_analysis.R_, transforms the raw set into the tidy set by:
+
+1. Combining the training and test datasets
+2. Linking the measurements to subject IDs
+3. Laveling each activity and feature
+4. Computing means and SDs for each activity, by subject and activity
+5. Reorganizing the data so that each feature/activity/subject/measure (mean or SD) combination is in its own row
+
+To use the script, download the raw dataset here: http://archive.ics.uci.edu/ml/machine-learning-databases/00240/
+
+Place the script into the main folder for the dataset, and set the working directory to the dataset folder. Then, simply run the script to generate "output.txt"--the tidy dataset.
+
+## Contents
+
+* _codebook.MD_: describes the fields in the tidy dataset
+* _output.txt_: the tidy dataset
+* _run_analysis.R_: contains the R script that tidies the raw data set
+
+
+## Dataset source
+
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 ## Set the working directory here
-setwd("C:/r/UCI HAR Dataset")
+library(reshape)
 
 combineData<-function() {
   
@@ -10,12 +10,12 @@ combineData<-function() {
 
   features<-read.table(file="features.txt")
   
-  trainingSubjects<-read.table("train/subject_train.txt",col.names="Subject")
-  trainingActivity<-read.table(file="train/y_train.txt",col.names = "Activity")
+  trainingsubjects<-read.table("train/subject_train.txt",col.names="subject")
+  trainingactivity<-read.table(file="train/y_train.txt",col.names = "activity")
   trainingSet<-read.table(file="train/X_train.txt",col.names=features[[2]])
   
-  testSubjects<-read.table("test/subject_test.txt",col.names="Subject")
-  testActivity<-read.table(file="test/y_test.txt",col.names="Activity")
+  testsubjects<-read.table("test/subject_test.txt",col.names="subject")
+  testactivity<-read.table(file="test/y_test.txt",col.names="activity")
   testSet<-read.table(file="test/X_test.txt",col.names=features[[2]])
   
   activityLabels<-read.table(file="activity_labels.txt")
@@ -23,41 +23,55 @@ combineData<-function() {
   ## Bind test and training data into a dataframe called harData
   
   harData<-rbind(
-    cbind(trainingSubjects,trainingActivity,trainingSet),
-    cbind(testSubjects,testActivity,testSet)
+    cbind(trainingsubjects,trainingactivity,trainingSet),
+    cbind(testsubjects,testactivity,testSet)
     )
   
   ## Assign labels to activities
-  harData$Activity<-factor(harData$Activity,levels=activityLabels[[1]],labels=activityLabels[[2]])
+  harData$activity<-factor(harData$activity,levels=activityLabels[[1]],labels=activityLabels[[2]])
   
   ## Clean up
   rm(features,
-     trainingActivity,
+     trainingactivity,
      trainingSet,
-     trainingSubjects,
-     testSubjects,
-     testActivity,
+     trainingsubjects,
+     testsubjects,
+     testactivity,
      testSet,
      activityLabels
   )
   
-  ## Remove all the columns except for means and standard deviations
-  
-  harData<-harData[grep(pattern="mean|std|Subject|Activity",x=colnames(harData),ignore.case=TRUE)]
   return(harData)
 }
 
 
-## This function takes the combined dataset and computes means by subject and activity.
-
-tidyData<-function(harData) {
+tidyData<-function(harData=harData) {
   
-  aggregate(x=harData[,-(1:2)],
-            by=list(Subject=harData$Subject,Activity=harData$Activity),
-            FUN="mean"
-            )
+  ## Remove all the columns except for means and standard deviations.
+  harData<-harData[grep(pattern="mean|std|subject|activity",x=colnames(harData),ignore.case=TRUE)]
+   
+  ## Take the combined dataset and compute means by subject and activity.
+  harData<-aggregate(x=harData[,-(1:2)],
+  
+          by=list(subject=harData$subject,activity=harData$activity),
+          FUN="mean"
+          )     %>%
+    
+  ## Change from wide to long form. (Each observation is its own row.)
+    melt(id=c("subject","activity"))
+    
+    ## Add a column for the type of observation: mean or standard deviation.
+    
+    harData$measure<NA  
+    harData$measure[grep(pattern="mean",x=harData$variable, ignore.case=T)]<-"mean"
+    harData$measure[grep(pattern="std",x=harData$variable, ignore.case=T)]<-"std"
+
+    ## remove "mean" or "std" from the variable columns, so variable names are uniform.
+    
+    harData$variable<-gsub(x=harData$variable, pattern = "mean|std",ignore.case = T,replacement = "")
+  
+  return(harData)
+
 }
 
-tidyData(combineData())
-
-
+write.table(tidyData(combineData()),"output.txt",row.names = F)
